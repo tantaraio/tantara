ff662d2e5ffa7f44cd05bfe20a6bf946b09642ca
Daw-Chih Liou <dawochih.liou@gmail.com>
2022-12-15 10:18:20 +0100

    test: add benchmark data and libraries
    GH-3

---
+++ benches/README.md
@@ -0,0 +1,5 @@
+# Benchmark
+
+The benchmark data and method is duplicated from [Tantivy](https://github.com/quickwit-oss/tantivy).
+
+> The benchmark and analysis is in progress.
---
+++ benches/index-bench.rs
@@ -0,0 +1,39 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use pprof::criterion::{Output, PProfProfiler};
+use tantivy::schema::{INDEXED, STRING, TEXT};
+use tantivy::Index;
+
+const HDFS_LOGS: &str = include_str!("hdfs.json");
+const NUM_REPEATS: usize = 2;
+
+pub fn hdfs_index_benchmark(c: &mut Criterion) {
+    let schema = {
+        let mut schema_builder = tantivy::schema::SchemaBuilder::new();
+        schema_builder.add_u64_field("timestamp", INDEXED);
+        schema_builder.add_text_field("body", TEXT);
+        schema_builder.add_text_field("severity", STRING);
+        schema_builder.build()
+    };
+
+    let mut group = c.benchmark_group("index-hdfs");
+    group.sample_size(20);
+    group.bench_function("index-hdfs-no-commit", |b| {
+        b.iter(|| {
+            let index = Index::create_in_ram(schema.clone());
+            let index_writer = index.writer_with_num_threads(1, 100_000_000).unwrap();
+            for _ in 0..NUM_REPEATS {
+                for doc_json in HDFS_LOGS.trim().split("\n") {
+                    let doc = schema.parse_document(doc_json).unwrap();
+                    index_writer.add_document(doc).unwrap();
+                }
+            }
+        })
+    });
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    targets = hdfs_index_benchmark
+}
+criterion_main!(benches);
