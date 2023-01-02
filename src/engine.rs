ad6c536e417271f83888a6efff8fc294179fc504
Daw-Chih Liou <dawochih.liou@gmail.com>
2023-01-02 14:35:37 +0100

    feat: basic engine
    GH-18

--- src/engine.rs
+++ src/engine.rs
@@ -1,36 +1,80 @@
+use tantivy::collector::TopDocs;
+use tantivy::query::QueryParser;
 use tantivy::schema::*;
+use tantivy::DocAddress;
+use tantivy::IndexReader;
 use tantivy::IndexWriter;
 use tantivy::ReloadPolicy;
-use tantivy::Searcher;
+use tantivy::Result;
 
-pub struct Registry {
+pub struct Index {
+    index: tantivy::Index,
+    schema: Schema,
+    fields: Vec<Field>,
     writer: IndexWriter,
-    searcher: Searcher,
+    reader: IndexReader,
 }
 
-pub fn create() -> tantivy::Result<Registry> {
+pub fn create() -> Result<Index> {
     let schema_builder = Schema::builder();
 
-    // TODO: add schema
+    // @TODO: create schema
+    // @TODO: create fields from schema input
 
+    let fields = vec![];
     let schema = schema_builder.build();
     let index = tantivy::Index::create_in_ram(schema.clone());
-
-    // index writer
     let writer = index.writer(50_000_000)?;
-
-    // index reader
     let reader = index
         .reader_builder()
         .reload_policy(ReloadPolicy::OnCommit)
         .try_into()?;
-    let searcher = reader.searcher();
 
-    Ok(Registry { writer, searcher })
+    Ok(Index {
+        index,
+        schema,
+        fields,
+        writer,
+        reader,
+    })
 }
 
-pub fn add() {}
+pub fn add(index: Index, docs: Vec<Document>) -> Result<Index> {
+    docs.iter()
+        .map(|doc| {
+            index.writer.add_document(doc.clone())?;
+            Ok(())
+        })
+        .collect::<Result<()>>()?;
 
-pub fn remove() {}
+    Ok(index)
+}
 
-pub fn search() {}
+pub fn commit(mut index: Index) -> Result<Index> {
+    index.writer.commit()?;
+    index.reader.reload()?;
+
+    Ok(index)
+}
+
+pub fn remove(mut index: Index, field: Field, text: &str) -> Result<Index> {
+    let term = Term::from_field_text(field, text);
+
+    index.writer.delete_term(term);
+
+    Ok(index)
+}
+
+pub fn search(
+    index: Index,
+    query: &str,
+    limit: Option<usize>,
+) -> tantivy::Result<Vec<(f32, DocAddress)>> {
+    let limit = limit.unwrap_or(10);
+    let parser = QueryParser::for_index(&index.index, index.fields);
+    let query = parser.parse_query(query)?;
+    let searcher = index.reader.searcher();
+    let result = searcher.search(&query, &TopDocs::with_limit(limit))?;
+
+    Ok(result)
+}
